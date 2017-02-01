5641a32ca0ced0d0a42c16f0f91fb1bbfc44dc69
Joao Santos <joaorsantos@gmail.com>
2017-02-01 18:05:51 -0500

    Cleaning data and exploration

--- Data/raw/AQI_daily/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ls aqidaily2015_*.csv | while read filename ;
-do
-	echo $filename;
-	sed -i 's/\"//g' $filename;
-	sed -i 's/,\.,/,,/g' $filename;
-	sed -i 's/,\.$/,/g' $filename;
-done
