291bd8358d6bf5c2a47d23518fd1259d1c84c324
Joao Santos <joaorsantos@gmail.com>
2017-02-06 14:54:04 -0500

    Converted weather data from weather station to county

--- Data/raw/weather/clean.sh
+++ Data/raw/weather/clean.sh
@@ -5,12 +5,7 @@ FILEOUT="${2}"
 
 cat ${FILEIN} | sed 's/\"//g' | sed 's/, [A-Z][A-Z] US//' > temp_GSOY.csv
 
-if [[ "${FILEIN#*GSOY}" == ".csv" ]] ; then
-  cut -d',' -f1,2,6- temp_GSOY.csv > "${FILEOUT}"
-  rm temp_GSOY.csv
-else
-  mv temp_GSOY.csv "${FILEOUT}"
-fi
+mv temp_GSOY.csv "${FILEOUT}"
 
 
 #TAVG, average temperature
